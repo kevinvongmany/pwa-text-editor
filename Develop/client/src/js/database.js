8f6a32a09ab6ea6b285e19229686d3bb5c4f6fd9
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-13 14:31:40 +1100

    Added update indexeddb functionality

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Updating database with:', content);
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ content });
+  const result = await request;
+  console.log('Database updated with:', result);
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+};
 
 initdb();
